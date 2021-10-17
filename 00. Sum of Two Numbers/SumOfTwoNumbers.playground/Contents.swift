e32d8580ad1aa9129f9f9cbf3e3cee2041114c0e
Neil Hiddink <nhiddink2@gmail.com>
2021-10-17 11:25:00 -0400

    Sum of Two Numbers

---
+++ 00. Sum of Two Numbers/SumOfTwoNumbers.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+// Sum of Two Numbers
+// Create a function that takes two numbers as arguments, then returns their sum.
+
+func sum(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+// Solution
+
+// Tests
+assert(sum(1, 1) == 2)
+
+print("🎉 Congratulations, all tests passed.")
